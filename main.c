dd0da3a5f659caff41f0d1587eabe9406f9efa91
Salt and Cereal <41021575+SaltyCereal@users.noreply.github.com>
2018-12-17 16:31:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 int main()
 {
+ {
   int i;
-  for (i = 0; i < 10; i++)
+  for(int i=0;i<16;i++) // loop will run n times (n=2) 
   {
-    printf("Holberton\n");
+      int n5 = fork();
+    printf("Hello Holberton\n");
   }
+    wait(NULL); 
   return 0;
+}
 }
